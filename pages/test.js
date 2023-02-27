5dfb610e61e4cd0e21e8b01baf95194d692ad924
SANJAY KUMAR V <70307085+sanjayv2002@users.noreply.github.com>
2023-02-27 06:43:10 +0000

    changes

--- pages/test.js
+++ pages/test.js
@@ -7,9 +7,9 @@ const Test = () => {
     const [data, setData] = useState('null');
 
     useEffect(() => {
-        fetch("url",{
+        fetch("https://api.elsevier.com/content/search/affiliation/60114413",{
             "Accept": "application/json",
-            "X-ELS-APIKey": "key"
+            "X-ELS-APIKey": "5ab1c755872c613fe61ae394b8d24ca4"
         }).then(res => res.json()).then(data => setData(data))
     }, []);
 
